6a303bbe1ed71271171b6449213643797278f9ca
sanikapareek-contentstack <sanika.pareek@contentstack.com>
2023-01-30 23:28:50 +0530

    Created banner component using dom manipulation

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import { Header } from "./components/header.js";
+import { Banner } from "./components/banner.js";
 
 const header=new Header();
 header.mount(document.getElementById("root"));
+
+const banner=new Banner();
+banner.mount(document.getElementById("root"));
--- src/components/banner.js
+++ src/components/banner.js
@@ -1,8 +1,24 @@
 import data from '../../data/banner.json' assert {type :'json'};
+import Component from './Mount.js';
 
-class Banner{
+
+class Banner extends Component{
     render(){
         const bannerContainer=document.createElement("div");
+        const bannerHeading=document.createElement("h1");
+        const bannerDescription=document.createElement("h2");
+
+        bannerContainer.classList.add("bannerContainer");
+
+        bannerContainer.appendChild(bannerHeading);
+        bannerContainer.appendChild(bannerDescription);
+
+        bannerHeading.innerText=data.name;
+        bannerDescription.innerText=data.position;
+
+        return bannerContainer;
         
     }
 }
+
+export {Banner};
