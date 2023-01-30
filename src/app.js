dce03700e0126a495e56a7dc27dc17ada6dfd4ac
sanikapareek-contentstack <sanika.pareek@contentstack.com>
2023-01-30 23:44:23 +0530

    Added profile image component using dom manipulation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import { Header } from "./components/header.js";
 import { Banner } from "./components/banner.js";
+import { ProfileImage } from "./components/profileImage.js";
 
 const header=new Header();
 header.mount(document.getElementById("root"));
 
 const banner=new Banner();
 banner.mount(document.getElementById("root"));
+
+const profileImg=new ProfileImage()
+profileImg.mount(document.getElementById("root"));
